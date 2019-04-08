20cbc436817d106a2bf6bbae2b40ca107fe9bae9
Saud <saudd2gh@gmail.com>
2019-04-08 09:05:09 +0400

    attractions section in landing page issue fixed

--- wp-content/themes/agency-theme/index.php
+++ wp-content/themes/agency-theme/index.php
@@ -32,16 +32,19 @@
       while($attractionsQuery->have_posts()){
         $attractionsQuery->the_post();
         $modalTargetId = get_the_ID().'-modal';
-        $columnLayout ='col s12 xl4';
+        
+
+        $columnLayout ='col s12 ';
 
         if($attractionsQuery->current_post < 2)
-          $columnLayout .= 'm6 l4';
+          $columnLayout .= 'm6 l4 xl4';
         elseif($attractionsQuery->current_post == 2)
-          $columnLayout .= 'm12 l4';
-        else
-          $columnLayout .= 'm6 l6';
+          $columnLayout .= 'm12 l4 xl4';
+        elseif($attractionsQuery->current_post > 2)
+          $columnLayout .= 'm6 l6 xl6';
         
-        if($attractionsQuery->current_post == 0 || $attractionsQuery->current_post == 3) echo '<div class="row">';
+        if($attractionsQuery->current_post == 0 || $attractionsQuery->current_post == 3)
+          echo '<div class="row">';
 ?>
           <div class="<?php echo $columnLayout ?>">
             <div class="card attr-card">
@@ -50,25 +53,25 @@
                 data-target="<?php echo $modalTargetId ?>">View</a>
               </div>
               <div class="card-image">
-                <img class="modal-trigger" <?php echo 'data-target="$modalTargetId"' ?>
+                <img class="modal-trigger responsive-img" data-target="$modalTargetId"
                 src="<?php the_post_thumbnail(); ?>">
                 <p class="card-title deep-orange accent-2">
-                  <a style="cursor: pointer;" class="modal-trigger amber-text text-lighten-5"
-                  data-target="<?php echo $modalTargetId ?>">
-                    <?php the_title(); ?>
-                  </a>
+                    <a style="cursor: pointer;" class="modal-trigger amber-text text-lighten-5"
+                    data-target="<?php echo $modalTargetId ?>"><?php the_title(); ?></a>
                 </p>
-              </div>
-            </div>
-            <div id="<?php echo $modalTargetId ?>" class="modal">
-              <div class="modal-content">
-                <h4><?php the_title(); ?></h4>
-                <img class="responsive-img" src="<?php the_post_thumbnail(); ?>">
-                <p class="left-align" style="padding: 0 20px"><?php the_content(); ?></p>
-              </div>
             </div>
           </div>
+          <div id="<?php echo $modalTargetId ?>" class="modal">
+            <div class="modal-content">
+              <h4><?php the_title(); ?></h4>
+              <img class="responsive-img" src="<?php the_post_thumbnail(); ?>">
+              <p class="left-align" style="padding: 0 20px"><?php the_content(); ?></p>
+            </div>
+          </div>
+        </div>
 <?php
+        get_template_part('template-parts/content', 'landing-attraction-card');
+
         if($attractionsQuery->current_post == 2 || $attractionsQuery->current_post == 4) echo '</div>';
 
       }
@@ -76,5 +79,41 @@
 ?>
     <a href="packages.html" class="btn-large waves-effect pink lighten-1">VIEW ATTRACTIONS</a>
   </div>
+
+  <div class="divider"></div>
+
+  <div id="packages-landing" class="center-align">
+    <h4>Our Packages</h4>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Doloremque, ipsum? Magnam reiciendis unde,
+      enim similique eos ipsa earum iste laboriosam saepe omnis
+      rerum tenetur harum ducimus illum quisquam sit dolor!
+    </p>
+<?php
+    $packagesQuery = new WP_Query(array(
+      'post_type' => 'package',
+      'posts_per_page' => 5
+    ));
+
+?>
+    <div class="row">
+      <div class="col xl6 l6 m12 s12">
+        <div class="card hoverable package-card modal-trigger" data-target="wahiba-modal">
+          <div class="card-image waves-effect">
+            <img class="responsive-img" src="img/camel-468096_1280.jpg">
+            <span class="badge red white-text deep-orange accent-2">$500</span>
+            <span class="card-title cyan-text text-lighten-1 left-align">
+              Wahiba Sands Tour<br>
+              5 Days/4 Night
+            </span>           
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+
 
 <?php get_footer(); ?>
